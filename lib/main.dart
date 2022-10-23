17c65c493a630a6f4aaa7044305869dfada6f6be
Guilherme Palma <guippalma@gmail.com>
2022-10-23 13:44:22 -0300

    fix: duplicate home screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
       routes: {
         AppRoutes.homeRoute: (ctx) => const HomeScreen(),
         AppRoutes.randomQuestions: (ctx) => const QuizLoadingScreen(),
