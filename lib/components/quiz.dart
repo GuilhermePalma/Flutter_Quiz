ec9fa467b27ac98fb1f4772d0e4d4fc869c1ca64
Guilherme Palma <guippalma@gmail.com>
2022-10-27 22:39:13 -0300

    feat: progress indicator animation

--- lib/components/quiz.dart
+++ lib/components/quiz.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../model/entities/quiz_entity.dart';
@@ -24,19 +26,38 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   bool isAnswered = false;
   bool isLoading = false;
+  double _valueProgress = 0;
   List<ButtonAnswer>? values;
 
   void changeIsAnswered() => setState(() => isAnswered = !isAnswered);
   void changeIsLoading({bool newValueIsLoading = false}) =>
       setState(() => isLoading = newValueIsLoading);
 
+  void setValueProgress({double value = 0}) =>
+      setState(() => _valueProgress = value);
+  void incrementValueProgress({double quantity = 1}) =>
+      setState(() => _valueProgress += quantity);
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        CustomText(widget.quizEntity.question),
-        ..._getResponsesButtons(),
+        isLoading
+            ? LinearProgressIndicator(
+                value: _valueProgress,
+                color: Colors.lightGreenAccent,
+              )
+            : const SizedBox(height: 4),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomText(widget.quizEntity.question),
+            const SizedBox(height: 16),
+            Column(children: _getResponsesButtons()),
+          ],
+        ),
+        const SizedBox(height: 4),
       ],
     );
   }
@@ -47,14 +68,29 @@ class _QuizState extends State<Quiz> {
             isCorrectAnswer: e.isCorrect,
             isAnswered: isAnswered,
             isLoading: isLoading,
-            functionPressed: () async {
+            functionPressed: () {
               changeIsAnswered();
               changeIsLoading(newValueIsLoading: true);
-              await Future.delayed(const Duration(seconds: 3), () {});
-              changeIsLoading(newValueIsLoading: false);
-              changeIsAnswered();
-              widget.clickButton(e.isCorrect);
+              progressIndicator(() => widget.clickButton(e.isCorrect));
             },
           ))
       .toList();
+
+  void progressIndicator(Function() afterFinishFunction) {
+    Timer.periodic(const Duration(milliseconds: 40), (Timer timer) {
+      if (_valueProgress >= 1) {
+        timer.cancel();
+        whenFinish(afterFinishFunction);
+        setValueProgress();
+      } else {
+        incrementValueProgress(quantity: 0.025);
+      }
+    });
+  }
+
+  void whenFinish(Function() clickButton) {
+    changeIsLoading(newValueIsLoading: false);
+    changeIsAnswered();
+    clickButton();
+  }
 }
